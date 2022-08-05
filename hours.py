2b52982e85c4846f5720f8c626abe4fa83cae6c3
ZachMora <110693820+ZachMora@users.noreply.github.com>
2022-08-05 16:05:27 -0700

    Add files via upload

---
+++ hours.py
@@ -0,0 +1,17 @@
+#calculate Gusto hours
+
+
+
+Day_1 = (int(input("How many minutes did you work Monday? ")))
+Day_2 = (int(input("How many minutes did you work this Tuesday? ")))
+Day_3 = (int(input("How many minutes did you work this Wednesday? ")))
+Day_4 = (int(input("How many minutes did you work this Thursday? ")))
+Day_5 = (int(input("How many minutes did you work this Friday? ")))
+
+workweek = [Day_1, Day_2, Day_3, Day_4, Day_5]
+
+total_minutes = sum(workweek)
+
+weekly_hours = total_minutes / 60
+
+print("You worked " + (str(float(weekly_hours))) + " hours")
